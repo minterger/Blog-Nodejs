82b5bb9e46d5f4a659e4320211b24224e969daf9
minterger <minterger@gmail.com>
2021-02-18 20:37:01 -0300

    Revert "helper hbs"
    This reverts commit ca0cb8a4a8f95306cce0f3b56c4d7ae359bcec1b.

--- src/helpers/hbs.js
+++
@@ -1,9 +0,0 @@
-const { create } = require('express-handlebars');
-const hbs = create({});
-
-hbs.handlebars.registerHelper('prueba', () => {
-    let hola = 'hola mundo'
-    return hola
-})
-
-module.exports = hbs
