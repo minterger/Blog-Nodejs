8367be9537d58dd472ac0c683f9facdf62c3b780
minterger <minterger@gmail.com>
2021-02-23 14:58:27 -0300

    update

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,15 +1,16 @@
 let menu = document.getElementById('menu');
 let navbar = document.getElementById('navbar');
-let container = document.getElementById('container')
+let container = document.getElementById('container');
 
 menu.addEventListener('click', () => {
     navbar.classList.toggle('navbar');
-    container.classList.toggle('margin-top')
+    container.classList.toggle('margin-top');
 });
 
-let alert = document.getElementById('alert')
+let alert = document.getElementById('alert');
+let close_btn = document.getElementById('close-btn');
 
-alert.addEventListener('click', () => {
-    alert.classList.toggle('alert-hiden')
+close_btn.addEventListener('click', () => {
+    alert.classList.toggle('alert-hiden');
 
-})
+});
